3151313a495196f7377c42a1b17db21bf19b851c
Lalmi-Issam <lalmiissammohammed2318@gmail.com>
2019-04-19 23:07:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ cors_proxy.createServer({
   originWhitelist: originWhitelist,
   requireHeader: [],
   checkRateLimit: checkRateLimit,
+  setHeaders: {'host':'khkhkhkh.com','user-agent': 'blablabla'}
   removeHeaders: [
     'cookie',
     'cookie2',
